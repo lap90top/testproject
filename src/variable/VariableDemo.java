db0a7f91340eb4d51ebe882ffde45a4295ff6041
lap90top <agalkagoyal@gmail.com>
2021-08-13 01:17:31 +0530

    hello

---
+++ src/variable/VariableDemo.java
@@ -0,0 +1,25 @@
+package variable;
+
+public class VariableDemo {
+    public static void main(String args []){
+
+      //  Syntax of variable:
+        //Data type variable_name= value;
+
+      String name = "Raj";
+      System.out.println(name);
+
+        String cityName = "Agra";
+        System.out.println(cityName );
+
+        int age = 25;
+        System.out.println(age);
+
+        double salary = 25567.30;
+        System.out.println(salary);
+
+        double price = 20.10;
+        System.out.println(price);
+    }
+
+}
