6b5db75282ce1f5eafafdc619a0327e43ffb32b6
lap90top <agalkagoyal@gmail.com>
2021-08-24 14:09:14 +0530

    Added code of collection for DemoList

--- src/array/DemoArray.java
+++ src/array/DemoArray.java
@@ -4,68 +4,94 @@ public class DemoArray {
 
     // syntax of signature method: access_specifier return_type function or methodName(optional parameter){code}
 
-    public void demo(){
+    public void demo() {
         // Data type: String
-   // syntax of Array: DataType [] variable_name ={,,,,,,,,};
-    String [] nameArray ={"Test","John","Raj","Rahul","Ankit","Ajay"};
-    System.out.println(nameArray[0]);
-    System.out.println(nameArray[1]);
-    System.out.println(nameArray[2]);
-    System.out.println(nameArray[3]);
-    System.out.println(nameArray[4]);
-    System.out.println(nameArray[5]);
+        // syntax of Array: DataType [] variable_name ={,,,,,,,,};
+        String[] nameArray = {"Test", "John", "Raj", "Rahul", "Ankit", "Ajay"};
+        System.out.println(nameArray[0]);
+        System.out.println(nameArray[1]);
+        System.out.println(nameArray[2]);
+        System.out.println(nameArray[3]);
+        System.out.println(nameArray[4]);
+        System.out.println(nameArray[5]);
 
     }
 
 
-        public void demo1(){
-       // Data type: String
-        String [] nameArray ={"Test","John","Raj","Rahul","Ankit","Ajay"};
+    public void demoStringArray(){
+        // Data type: String
+        String[] nameArray = {"Test", "John", "Raj", "Rahul", "Ankit", "Ajay"};
 
         // syntax for normal for_loop: for(int i=0; i<variable_name.length; i++){}
-            for(int i= 0;i<nameArray.length;i++){
+        for (int i = 0; i < nameArray.length; i++) {
 
-              System.out.println("Printing Array :" +nameArray[i] + "Printing index i :" +i);
-
-
-            }
+            System.out.println("Printing Array :" + nameArray[i] + "Printing index i :" + i);
 
 
-       // syntax for Advance for loop: for (DataType var :ArrayName){}
+        }
 
-            for (String var :nameArray){
+        // syntax of Advance for loop: for (DataType variable :ArrayName or Array/Collection){}
 
-            System.out.println("Inside nameArray Advance loop :" +var);
+        for (String var : nameArray){
 
-            }
+            System.out.println("Inside nameArray Advance loop :" + var);
 
-            // Data type: int
+        }
+
+    }
+
+
+        // Data type: int
+        public void demoIntArray () {
+
             // syntax of Array: DataType [] variable_name ={,,,,,,,,};
+            // symbol of Array:[]
 
-            int [] marksArray = {24,36,60,70,40,30,20,10,45};
-            // syntax for Advance for loop: for (DataType var :ArrayName){}
-            for (int var : marksArray){
+            int[] marksArray = {24, 36, 60, 70, 40, 30, 20, 10, 45};
+            // syntax of Advance for loop: for (DataType variable :ArrayName or Array/Collection){}
+            //here ArrayName----marksArray
 
-               System.out.println("Inside marksArray Advance loop:" +var);
+            for (int var : marksArray) {
 
-               }
-
+                System.out.println("Inside marksArray Advance loop:" + var);
 
 
             }
+        }
 
 
 
-        public static void main(String[] args) {
+            // Data type: double
+    public void demoDoubleArray(){
 
-        //step1: we will create an object of a class (here our class is student)------- className objectName =new className();
-        // step2:object.methodName or functionName();   ---------------------for calling or executing a method through main method.
+        double[] prices = {20.50, 10.20, 50.30, 60.40, 80.20};
 
-        DemoArray object = new DemoArray();
-        object.demo();
-        object.demo1();
+        for (double var : prices) {
+
+            System.out.println("Printing prices Array :" +var);
+
+
+        }
 
 
     }
+
+
+
+        public static void main (String[]args){
+
+            //step1: we will create an object of a class (here our class is student)------- className objectName =new className();
+            // step2:object.methodName or functionName();   ---------------------for calling or executing a method through main method.
+
+            DemoArray object = new DemoArray();
+            object.demo();
+            object.demoStringArray();
+            object.demoIntArray();
+            object.demoDoubleArray();
+
+
+
+        }
+
 
 }
---
+++ src/collection_demo/DemoList.java
@@ -0,0 +1,69 @@
+package collection_demo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DemoList {
+
+// syntax of signature method:access_specifier return_type function or methodName (optional parameter){code}
+
+    public void demo() {
+
+        // Method---add(object),remove(index),get(index)
+        // ArrayList is a class,when we will create an object of class then we can add data in it.
+        // ArrayList is already a class in java where these methods such as add,remove,get have written only we can use it.
+        // List----parent class, ArrayList-------child class, it means in parent class ,child class will be included.
+
+        //add method:
+        //syntax of list :ArrayList or List  object_name = new ArrayList();
+
+        List list = new ArrayList();
+        list.add("Rahul");  //0 index         // here we are taking data in String form ( "Rahul","Raj","Ajay")
+        list.add("Raj");    //1 index
+        list.add("Ajay");   //2 index
+
+
+        //get method : it is used for obtaining a particular data from the list
+        System.out.println("Printing element using get :" + list.get(0));
+        System.out.println("Printing element using get :" + list.get(1));
+        System.out.println("Printing element using get :" + list.get(2));
+
+
+        //Advance for loop: it is used for printing the data value (Rahul, Raj,Ajay).
+        // syntax of Advance for loop:for(DataType variable : ArrayName or Array/Collection){code}
+        // here we are taking an Object as a DataType because an Object is a parent class
+        // in which we can put everything such as String,list etc.
+        // variable (var) ------this contains value of list (in which name of list will come one by one)
+
+        for (Object var : list) {
+            System.out.println(var);
+        }
+
+        // remove method:
+        list.remove(0);
+
+        // syntax of Advance for loop:for(DataType variable : ArrayName or Array/Collection){code}
+        for (Object var : list) {
+            System.out.println("after delete :" + var);
+        }
+
+
+
+
+    }
+
+    public static void main(String[] args) {
+
+        //step1: we will create an object of a class (here our class is student)------- className objectName =new className();
+        // step2:object.methodName or functionName();   ---------------------for calling or executing a method through main method
+
+        DemoList object = new DemoList();
+        object.demo();
+
+
+    }
+
+
+
+
+}
